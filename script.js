422ec7e44986ac2edebb50d04612efa4d7189163
Jacob N <177889821+baaramuser123@users.noreply.github.com>
2025-02-17 18:36:00 -0500

    -Add playGame function to facilitate full game consisting of 5 rounds -Shift playRound function into playGame -Improve getComputerChoice function's random choice logic for better variance

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 console.log('Hello World!');
+alert("You are about to play 5 rapid rounds of Rock Paper Scissors against an AI. You will see the results in the Developer Console.")
+playGame();
 
 // getComputerChoice function requirement: randomly return one of the following string values: “rock”, “paper” or “scissors”.
 // pseudocode:
@@ -9,7 +11,7 @@ console.log('Hello World!');
 
 function getComputerChoice () {
     let weaponNumber;
-    weaponNumber = Math.floor(Math.random() *3) + 1;
+    weaponNumber = Math.round(Math.random() *2) + 1;
     // console.log(weaponNumber);
     switch (weaponNumber) {
         case 1:
@@ -39,56 +41,87 @@ function getHumanChoice() {
     }
 }
 
-// Create two new variables named humanScore and computerScore in the global scope.
-// Initialize those variables with the value of 0.
-let humanScore = 0;
-let computerScore = 0;
+// Pseudocode
+// Expected Output: Play 5 Games and display the score at the end
+// Required Input: score is already tracked, as are individual games. Require that input is newly collected each game, and score is retained.
 
-// Your game will be played round by round. You will write a function that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
 
-// Create a new function named playRound.
-// Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take the human and computer choices as arguments.
-// Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
-// Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
-// Increment the humanScore or computerScore variable based on the round winner.
+function playGame() {
 
-function playRound(humanChoice, computerChoice) {
-    let result;
-    if(humanChoice=='scissors'){
-        if(computerChoice=='rock'){
-            result='Computer Wins';
-        } else if(computerChoice=='paper'){
-            result='You Win';
-        } else {
-            result ='It\'s a draw';
-        }
-    } else if(humanChoice=='rock'){
-        if(computerChoice=='paper'){
-            result='Computer Wins';
-        } else if(computerChoice=='scissors'){
-            result='You Win';
-        } else {
-            result ='It\'s a draw';
-        }
-    } else if(humanChoice=='paper'){
-        if(computerChoice=='scissors'){
-            result='Computer Wins';
-        } else if(computerChoice=='rock'){
-            result='You Win';
-        } else {
-            result ='It\'s a draw';
-        }
-    } else{
-        result = "Nothing Entered, please try again."
+    // Create two new variables named humanScore and computerScore in the global scope.
+    // Initialize those variables with the value of 0.
+    let humanScore = 0;
+    let computerScore = 0;
+
+
+    for(let i =1; i < 6; i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
     }
-    console.log(result);
+    console.log('Final score is Human: ' + humanScore + ' Computer: ' + computerScore);
+ 
 
-  }
+
+
+    // Your game will be played round by round. You will write a function that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
+
+    // Create a new function named playRound.
+    // Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take the human and computer choices as arguments.
+    // Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
+    // Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
+    // Increment the humanScore or computerScore variable based on the round winner.
+
+    function playRound(humanChoice, computerChoice) {
+        let result;
+        console.log('Human plays ' + humanChoice);
+        console.log('Computer plays ' + computerChoice);
+        if(humanChoice=='scissors'){
+            if(computerChoice=='rock'){
+                result='Computer Wins';
+                computerScore++;
+            } else if(computerChoice=='paper'){
+                result='You Win';
+                humanScore++;
+            } else {
+                result ='It\'s a draw';
+            }
+        } else if(humanChoice=='rock'){
+            if(computerChoice=='paper'){
+                result='Computer Wins';
+                computerScore++;
+            } else if(computerChoice=='scissors'){
+                result='You Win';
+                humanScore++;
+            } else {
+                result ='It\'s a draw';
+            }
+        } else if(humanChoice=='paper'){
+            if(computerChoice=='scissors'){
+                result='Computer Wins';
+                computerScore++;
+            } else if(computerChoice=='rock'){
+                result='You Win';
+                humanScore++;
+            } else {
+                result ='It\'s a draw';
+            }
+        } else{
+            result = "Nothing Entered, please try again."
+        }
+        console.log(result);
+        console.log('Current score is Human: ' + humanScore + ' Computer: ' + computerScore);
   
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice();
-  console.log('Human plays ' + humanSelection);
-  console.log('Computer plays ' + computerSelection);
+
+    }
+
+}
   
-  playRound(humanSelection, computerSelection);
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
+// console.log('Human plays ' + humanSelection);
+// console.log('Computer plays ' + computerSelection);
+
+// playRound(humanSelection, computerSelection);
+
   
