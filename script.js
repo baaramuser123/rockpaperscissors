bf21ed02f88f57ad64ee9dbf4cf77154e833b380
Jacob N <177889821+baaramuser123@users.noreply.github.com>
2025-03-10 16:45:51 -0400

    -Add Basic UI display for game -Refactor JS to accept input from buttons -Refactor JS to load after DOM

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-console.log('Hello World!');
-alert("You are about to play 5 rapid rounds of Rock Paper Scissors against an AI. You will see the results in the Developer Console.")
-playGame();
 
-// getComputerChoice function requirement: randomly return one of the following string values: “rock”, “paper” or “scissors”.
-// pseudocode:
-// 1. a variable named "weaponNumber" will be initialized
-// 2. weaponNumber will be set to a random number multiplied by 3 and then given a floor, plus 1, to give a value 1, 2, or 3.
-// 3. switch statement will be called to evaluate based on weaponNumber's value.
-// 4. for value 1, return rock, for value 2, return paper, and for value 3, return scissors
+// playGame();
+document.addEventListener('DOMContentLoaded', () => {
+    const rock = document.querySelector("#rock");
+    const paper = document.querySelector("#paper");
+    const scissors = document.querySelector("#scissors");
+    const buttons = document.querySelector(".buttons");
+    
+    console.log(buttons);
+    console.log(rock);
+
+// buttons.addEventListener("click", (e) => console.log(e.target.id));
+
+buttons.addEventListener("click", (e) => playRound(e.target.id, getComputerChoice()))
 
 function getComputerChoice () {
     let weaponNumber;
@@ -23,11 +27,6 @@ function getComputerChoice () {
     }
 }
 
-// getHumanChoice function requirement: use "prompt" and return r/p/s string
-// pseduocode:
-// 1. function will be called in console to start Game
-// 2. Prompt will appear to ask for numbers 1-3
-// 3. prompt number will be translated in switch to r/p/s.
 
 function getHumanChoice() {
     let humanInput = prompt("Please Enter a Number between 1-3. 1 for rock, 2 for paper, 3 for scissors.");
@@ -41,25 +40,21 @@ function getHumanChoice() {
     }
 }
 
-// Pseudocode
-// Expected Output: Play 5 Games and display the score at the end
-// Required Input: score is already tracked, as are individual games. Require that input is newly collected each game, and score is retained.
-
-
-function playGame() {
+// function playGame() {
 
     // Create two new variables named humanScore and computerScore in the global scope.
     // Initialize those variables with the value of 0.
     let humanScore = 0;
     let computerScore = 0;
+    let roundCount = 1;
 
 
-    for(let i =1; i < 6; i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-    console.log('Final score is Human: ' + humanScore + ' Computer: ' + computerScore);
+    // for(let i =1; i < 6; i++){
+    //     const humanSelection = getHumanChoice();
+    //     const computerSelection = getComputerChoice();
+    //     playRound(humanSelection, computerSelection);
+    // }
+    // console.log('Final score is Human: ' + humanScore + ' Computer: ' + computerScore);
  
 
 
@@ -115,13 +110,5 @@ function playGame() {
 
     }
 
-}
-  
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
-// console.log('Human plays ' + humanSelection);
-// console.log('Computer plays ' + computerSelection);
-
-// playRound(humanSelection, computerSelection);
-
-  
+// }
+});
